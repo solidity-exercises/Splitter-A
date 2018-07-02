552c1e22da1ec3e432d8667131477e34973a23b7
stringie <anthony.kaloferov@gmail.com>
2018-07-02 19:47:00 +0300

    Added functionality for adding a member

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -11,6 +11,7 @@ contract Splitter is Destructible {
         bool isMember;
         uint donations;
         uint contractBalanceAtLastWithdraw;
+        uint contractBalanceAtCreation;
     }
 
     mapping (address=>Member) public members;
@@ -26,7 +27,7 @@ contract Splitter is Destructible {
         require(initialMembers.length >= 3);
         
         for (uint i = 0; i < initialMembers.length; i++) {
-            members[initialMembers[i]] = Member({isMember: true, donations: 0, contractBalanceAtLastWithdraw: 0});
+            members[initialMembers[i]] = Member({isMember: true, donations: 0, contractBalanceAtLastWithdraw: 0, contractBalanceAtCreation: totalBalance});
         }
 
         memberCount = initialMembers.length;
@@ -41,11 +42,23 @@ contract Splitter is Destructible {
     function getMemberBalance(address _member) public view returns (uint) {
         require(members[_member].isMember);
 
-        return (getContractBalance() - members[_member].donations - members[_member].contractBalanceAtLastWithdraw) / (memberCount - 1);
+        return (getContractBalance() - 
+                members[_member].donations - 
+                members[_member].contractBalanceAtLastWithdraw -
+                members[_member].contractBalanceAtCreation) / 
+                (memberCount - 1);
     }
 
     function getContractBalance() public view returns (uint) {
         return address(this).balance;
+    }
+
+    function addMember(address _newMember) public onlyOwner {
+        _addArtificialBalancer();
+
+        memberCount += 1;
+
+        members[_newMember] = Member({isMember: true, donations: 0, contractBalanceAtLastWithdraw: 0, contractBalanceAtCreation: totalBalance});
     }
 
     function withdraw() public onlyMembers {
@@ -62,5 +75,13 @@ contract Splitter is Destructible {
 
     function _canWithdraw(address _members) private view returns (bool) {
         return members[_members].contractBalanceAtLastWithdraw != totalBalance;
+    }
+
+    // When adding new member this function is called so as to make the mathematical logic
+    // behind the contract valid. Without this function the getMemberBalance function will not
+    // return the correct value since memberCount is bigger by one. This function does not affect the
+    // contract balance, only the logic
+    function _addArtificialBalancer() private {
+        totalBalance += (totalBalance / memberCount);
     }
 }
